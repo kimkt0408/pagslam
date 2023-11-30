258186d36ee80ddf676bc586c3b99be38f609670
Kitae Kim <kimkt0408@gmail.com>
2023-11-30 16:52:51 -0500

    .

--- src/inputNode_.cpp
+++ src/inputNode_.cpp
@@ -254,7 +254,6 @@ bool InputManager::callPAGSLAM(SE3 relativeMotion, StampedSE3 odom)
         pcl_conversions::toPCL(odom.stamp, h_cloud->header.stamp);
         pcl_conversions::toPCL(odom.stamp, v_cloud->header.stamp);
         
-
         // std::cout << "prevKeyPose matrix:\n" << prevKeyPose.matrix() << std::endl;
         // std::cout << "relativeMotion matrix:\n" << relativeMotion.matrix() << std::endl;
 
