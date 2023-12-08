6420f84aeade702c2c122c2c4ae1adf5a9fb4c62
Kitae Kim <kimkt0408@gmail.com>
2023-12-08 11:56:22 -0500

    Modified xyyaw optimizer
    From 3d to 2d

--- include/definitions.h
+++ include/definitions.h
@@ -227,7 +227,13 @@ struct XYYawLineCost {
 
         for (const auto& pointInCloud : modelCloud.points) {
             Eigen::Matrix<T, 3, 1> cloud_point{T(pointInCloud.x), T(pointInCloud.y), T(pointInCloud.z)};
-            T distance = (cloud_point - last_point).norm();
+            // Create a 2D vector (Eigen::Matrix) from the first two elements
+            Eigen::Matrix<T, 2, 1> cloud_point_xy, last_point_xy;
+
+            cloud_point_xy << cloud_point(0), cloud_point(1);
+            last_point_xy << last_point(0), last_point(1);
+            
+            T distance = (cloud_point_xy - last_point_xy).norm();
             if (distance < minDistance) {
                 minDistance = distance;
             }
