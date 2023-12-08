bbb330e937cf2c3cd3b54cf59dc802d4b4baf816
Kitae Kim <kimkt0408@gmail.com>
2023-12-08 09:07:34 -0500

    Revert "Best parameters"
    This reverts commit 587a4578dfbd0dd14da15bc988c3278dca8c8cd5.

--- src/pagslam_.cpp
+++ src/pagslam_.cpp
@@ -49,10 +49,15 @@ namespace pagslam
         stalkMatchThresh_ = 0.50;  // 0.12 // 0.3
         minStalkMatches_ = 3; //2;
         rangeGroundMatch_ = 10; // 4
+<<<<<<< HEAD
         AddNewStalkThreshDist_ = stalkMatchThresh_*0.1;
         SkipStalkThreshDist_ = AddNewStalkThreshDist_*2.0;
+=======
+        AddNewStalkThreshDist_ = stalkMatchThresh_*0.3;
+        SkipStalkThreshDist_ = stalkMatchThresh_;
+>>>>>>> parent of 587a457 (Best parameters)
         maxNumIterations_ = 1000;
-        huberLossThresh_ = 1; // 0.1
+        huberLossThresh_ = 10; // 0.1
     }
 
 
